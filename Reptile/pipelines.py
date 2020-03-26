2eff2110f2998beb2464b816b5637813e2f9279a
CSLHouse <774038400@qq.com>
2020-03-26 12:21:00 +0800

    first commit

---
+++ Reptile/pipelines.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+from openpyxl import Workbook
+class ReptilePipeline(object):
+    def __init__(self):
+        self.wb = Workbook()
+        self.ws = self.wb.active
+        self.ws.append(['标题', '公司', '价格', '销量', 'method', 'rebuy', '地址', '保险'])
+
+    def process_item(self, item, spider):
+        line = [item['title'], item['company'], item['price'], item['sell'], item['method'], item['rebuy'], item['address'], item['subicon']]
+        self.ws.append(line)
+        self.wb.save('1688.xlsx')
+
+    def close_spider(self, spider):
+        self.wb.close()
---
+++ Reptile/spiders/a1688.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from bs4 import BeautifulSoup
+from Reptile.items import ReptileItem
+
+KEYWORD = "春夏女装"
+PAGE='1'
+class A1688Spider(scrapy.Spider):
+    def __init__(self):
+        self.item = ReptileItem()
+        self.item['title'] = ''
+        self.item['company'] = ''
+        self.item['price'] = ''
+        self.item['sell'] = ''
+        self.item['rebuy'] = ''
+        self.item['method']  = ''
+        self.item['address'] = ''
+        self.item['subicon'] = ''
+
+    name = '1688'
+    # allowed_domains = ['s.1688.com']
+    # start_urls = ['http://s.1688.com/']
+    def start_requests(self):
+        for page in range(1, int(PAGE)+1):
+            
+            url = 'https://s.1688.com/selloffer/offer_search.htm?keywords=%s&n=y&netType=16&beginPage=%s#sm-filtbar' % (KEYWORD, page)
+            yield scrapy.Request(url=url, callback=self.parse, encoding='gb2312')
+
+    def parse(self, response):
+        
+        for tag in response.css('.sw-dpl-offer-item').extract():
+            try:
+                # 从response中利用css选择器提取出来的标签是文本形式，需要利用 BeautifulSoup 转换成BeautifulSoup.Tag 对象进行进一步提取。
+                soup = BeautifulSoup(tag, 'lxml')
+
+                self.item['title'] = soup.select(".sw-dpl-offer-photo img")[0].attrs['alt']
+                self.item['company'] = soup.select(".sw-dpl-offer-companyName")[0].attrs['title']
+                self.item['price'] = soup.select(".sw-dpl-offer-priceNum")[0].attrs['title']
+                self.item['sell'] = soup.select(".sm-offer-tradeBt")[0].attrs['title']
+                self.item['rebuy'] =soup.select(".sm-widget-offershopwindowshoprepurchaserate span")[2].string
+                self.item['method']  = soup.select(".sm-widget-offershopwindowshoprepurchaserate i")[0].string
+                #对于不一定能获取的数据，需要判断数据存在与否。
+                if soup.select(".sm-offer-location")[0].attrs['title']:
+                    address= soup.select(".sm-offer-location")[0].attrs['title']
+                else:
+                    address = ""
+                self.item['address'] = address
+
+                if soup.select(".sm-offer-subicon a"):
+                    subicon = []
+                    for i in soup.select(".sm-offer-subicon a"):
+                        subicon.append(i.attrs['title'] + ',')
+                    subicon = "".join(subicon)
+                    self.item['subicon']=subicon
+                else:
+                    self.item['subicon'] = ' '
+                #返回这个数据模型，交给 ITEM_PIPELINES 处理
+                print("---------------sucsess-item:", dict(self.item))
+                yield self.item
+            except Exception as error:
+                print("--------------error--item:", dict(self.item))
+                yield self.item
+                print("出错了:", error)
+                continue
